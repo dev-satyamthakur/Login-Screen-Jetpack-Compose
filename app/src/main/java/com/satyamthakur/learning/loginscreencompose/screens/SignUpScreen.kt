e1f06baa80367d56d9cc6d00877513f97aa690f6
Satyam Thakur <sat.thakur20@gmail.com>
2024-01-01 20:31:37 +0530

    Creating signup screen composable with Greet Text

---
+++ app/src/main/java/com/satyamthakur/learning/loginscreencompose/screens/SignUpScreen.kt
@@ -0,0 +1,49 @@
+package com.satyamthakur.learning.loginscreencompose.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SignUpScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(16.dp)
+        ) {
+            GreetTextView(value = "Hey there,")
+        }
+    }
+}
+
+@Composable
+fun GreetTextView(value: String) {
+    Text(
+        modifier = Modifier.fillMaxWidth(),
+        text = value,
+        style = TextStyle(
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Normal
+        ),
+        textAlign = TextAlign.Center
+    )
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun DefaultPrev() {
+    SignUpScreen()
+}
